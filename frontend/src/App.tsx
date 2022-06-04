0392dd008630c4bd3ce9b289d1dd386b6a0b5057
Flavio Rogerio <solebac@hotmail.com>
2022-06-04 00:50:07 -0300

    Project Clean

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Projetos Web 100% tecnologia Java.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Java na pratica...!
-        </a>
-      </header>
-    </div>
+    <h1>Java na pratica...!</h1>
   );
 }
 
