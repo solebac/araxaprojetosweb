08d4f1a08cd494ad05a0298395991d7d66f18591
Flavio Rogerio <solebac@hotmail.com>
2022-06-07 02:20:15 -0300

    Create BannerHome

---
+++ frontend/src/components/Capa/index.tsx
@@ -0,0 +1,22 @@
+import * as React from "react";
+
+const Capa = () => {
+  return (
+    <section className="capa">
+      <div className="texto-capa">
+        <p className="titulo">
+          Desenvolvimento de site <span className="texto-modifica">100%</span>{" "}
+          tecnologia Java
+        </p>
+        <p className="subtitulo">Conceito de site inteligente.</p>
+        <a href="#" className="btn btn-custom btn-left btn-lg">
+          Demonstre sua marca na internet
+        </a>
+        <a href="#" className="btn btn-custom btn-rigth btn-lg">
+          Atualize você mesmo seu site.
+        </a>
+      </div>
+    </section>
+  );
+};
+export default Capa;
---
+++ frontend/src/components/Navbar/index.tsx
@@ -0,0 +1,76 @@
+import * as React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-fixed-top navbar-inverse navbar-transparente">
+      <div className="container-fluid">
+        <div className="navbar-header">
+          <button
+            type="button"
+            className="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#barra-navigation"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Alterna Navagação</span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+            <span className="icon-bar"></span>
+          </button>
+          <a className="navbar-brand" href="index.html">
+            <span className="img-logo">Araxá Projetos Web</span>
+          </a>
+        </div>
+
+        <div className="collapse navbar-collapse" id="barra-navigation">
+          <ul className="nav navbar-nav navbar-right">
+            <li>
+              <a href="/index.html">Home</a>
+            </li>
+            <li>
+              <a href="/portifolio">Portifólio</a>
+            </li>
+            <li className="dropdown">
+              <a
+                href="#"
+                className="dropdown-toggle"
+                data-toggle="dropdown"
+                role="button"
+                aria-haspopup="true"
+                aria-expanded="false"
+              >
+                Hacker <span className="caret"></span>
+              </a>
+              <ul className="dropdown-menu">
+                <li>
+                  <a href="/ethical.html">Ethical</a>
+                </li>
+                <li>
+                  <a href="/attack.html">Attack</a>
+                </li>
+                <li>
+                  <a href="/cofre.html">Cofre Virus</a>
+                </li>
+              </ul>
+            </li>
+
+            <li>
+              <a href="/blog.html">Blog</a>
+            </li>
+            <li>
+              <a href="/contato.html">Contato</a>
+            </li>
+            <li className="divisor" role="separator"></li>
+            <li>
+              <a href="#">Increver-se</a>
+            </li>
+            <li>
+              <a href="#">Entrar</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+export default Navbar;
