88cfc1018d4d18022fb063e6801cec24481b591c
Flavio Rogerio <solebac@hotmail.com>
2022-06-08 18:24:44 -0300

    Page 404|Modifield Router-Dom{Switch}

---
+++ frontend/src/pages/Pagina404.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "../assets/css/404.css";
+
+const Pagina404 = () => {
+  return (
+    <main className="container flex flex--centro flex--coluna">
+      <div className="error-404">
+        <div className="error-code m-b-10 m-t-20">
+          404 <i className="fa fa-warning"></i>
+        </div>
+        <h2 className="font-bold">
+          Oops 404! Essa página não pode ser encontrada.
+        </h2>
+
+        <div className="error-desc">
+          Desculpe, mas a página que você está procurando não foi encontrada ou
+          não existe. <br />
+          Tente atualizar a página ou clique no botão abaixo para voltar à
+          página inicial.
+          <div>
+            <br />
+            <a href="/" className="btn btn-primary">
+              <span className="glyphicon glyphicon-home"></span>
+              <span className="ml-4">Voltar para a página incial...!</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Pagina404;
