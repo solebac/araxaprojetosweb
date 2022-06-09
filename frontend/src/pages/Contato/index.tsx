eb8bab5a82fc4d5026f0bd03e8a9ea68e9b0df04
Flavio Rogerio <solebac@hotmail.com>
2022-06-09 10:33:27 -0300

    Create Contato

--- frontend/src/pages/Contato/index.tsx
+++ frontend/src/pages/Contato/index.tsx
@@ -3,12 +3,16 @@ import "../../assets/css/navs.css";
 import "../../assets/css/home.css";
 import Navbar from "../../components/Navbar";
 import BannerContato from "../../components/BannerContato";
+import MainContato from "../../components/MainContato";
+import Footers from "../../components/Footers";
 
 const Contato = () => {
   return (
     <>
       <BannerContato />
       <Navbar />
+      <MainContato />
+      <Footers />
     </>
   );
 };
--- frontend/src/pages/Portifolio/index.tsx
+++ frontend/src/pages/Portifolio/index.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+
 const Portifolio = () => {
   return <h1>Em desenvolvimento...!"</h1>;
 };
