6d6d6c09a19a612650108e0e42eb3ed84cb39e9d
Flavio Rogerio <solebac@hotmail.com>
2022-06-22 18:19:40 -0300

    Authorization Basic

--- backend/src/main/java/com/araxaprojetosweb/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/araxaprojetosweb/backend/config/SecurityConfig.java
@@ -26,53 +26,35 @@ import com.araxaprojetosweb.backend.entities.services.AutenticationService;
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private Environment env;
-	
+
 	@Autowired
 	private AutenticationService autenticationService;
-	
-	//Configuracoes de autenticacao
+
+	// Configuracoes de autenticacao
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		auth.userDetailsService(autenticationService)
-		.passwordEncoder(encoder());
+		auth.userDetailsService(autenticationService).passwordEncoder(encoder());
 	}
-	
-	//Configuração de Autorizacao
+
+	// Configuração de Autorizacao
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
 			http.headers().frameOptions().disable();
 		}
 
-		http.authorizeRequests().antMatchers(HttpMethod.POST, "/auth").permitAll()
-				.antMatchers("/h2-console", "/h2-console/**").permitAll().antMatchers(HttpMethod.GET, "/").permitAll()
+		http
+				.authorizeRequests()
+				.antMatchers(HttpMethod.POST, "/auth").permitAll()
+				.antMatchers("/h2-console", "/h2-console/**").permitAll()
+				.and()
+				.authorizeRequests()
+				.anyRequest().authenticated().and()
+				.cors().and()
+				.httpBasic().and().csrf().disable()
+				.sessionManagement()
+				.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 
-				.antMatchers(HttpMethod.GET, "/tag", "/tag/**").permitAll().antMatchers(HttpMethod.POST, "/tag")
-				.permitAll().antMatchers(HttpMethod.PUT, "/tag/**").permitAll()
-				.antMatchers(HttpMethod.DELETE, "/tag/**").permitAll()
-
-				.antMatchers(HttpMethod.GET, "/autor", "/autor/**").permitAll().antMatchers(HttpMethod.POST, "/autor")
-				.permitAll().antMatchers(HttpMethod.PUT, "/autor/**").permitAll()
-				.antMatchers(HttpMethod.DELETE, "/autor/**").permitAll()
-
-				.antMatchers(HttpMethod.GET, "/categoria", "/categoria/**").permitAll()
-				.antMatchers(HttpMethod.POST, "/categoria").permitAll().antMatchers(HttpMethod.PUT, "/categoria/**")
-				.permitAll().antMatchers(HttpMethod.DELETE, "/categoria/**").permitAll()
-				
-				.antMatchers(HttpMethod.GET, "/comment", "/comment/**").permitAll()
-				.antMatchers(HttpMethod.POST, "/comment").permitAll().antMatchers(HttpMethod.PUT, "/comment/**")
-				.permitAll().antMatchers(HttpMethod.DELETE, "/comment/**").permitAll()
-				
-				.antMatchers(HttpMethod.GET, "/articles", "/articles/**").permitAll()
-				.antMatchers(HttpMethod.POST, "/articles").permitAll().antMatchers(HttpMethod.PUT, "/articles/**")
-				.permitAll().antMatchers(HttpMethod.DELETE, "/articles/**").permitAll()
-
-				.antMatchers(HttpMethod.GET, "/artigocategoria").permitAll()
-				
-				.anyRequest().authenticated().and().cors().and()
-				.httpBasic().and().csrf().disable().sessionManagement()
-				.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-				.and().formLogin();
 	}
 
 	@Override
@@ -94,9 +76,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	public PasswordEncoder encoder() {
 		return new BCryptPasswordEncoder();
 	}
-	
-	/*public static void main(String[] args) {
-		System.out.println(new BCryptPasswordEncoder().encode("123"));
-	}*/
+
+	/*
+	 * public static void main(String[] args) { System.out.println(new
+	 * BCryptPasswordEncoder().encode("123")); }
+	 */
 
 }
