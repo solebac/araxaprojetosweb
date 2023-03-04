da8bce073abd8b7cfcb9ba45890e45d661aceed6
Flavio Rogerio <solebac@hotmail.com>
2023-03-04 03:06:19 -0300

    deleted lixo

--- src/main/java/com/araxaprojetosweb/backend/entities/enums/ArtigoStatus.java
+++
@@ -1,7 +0,0 @@
-package com.araxaprojetosweb.backend.entities.enums;
-
-public enum ArtigoStatus {
-	RESCUNHO,
-	AGENDADO,
-	PUBLICADO
-}
--- src/main/java/com/araxaprojetosweb/backend/repositories/ArtigoRepository.java
+++
@@ -1,9 +0,0 @@
-package com.araxaprojetosweb.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.araxaprojetosweb.backend.entities.Artigo;
-
-public interface ArtigoRepository extends JpaRepository<Artigo, Long>{
-
-}
