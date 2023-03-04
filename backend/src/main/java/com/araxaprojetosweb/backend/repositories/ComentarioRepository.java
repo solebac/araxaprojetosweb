83af845ced6421ddfbeb75a9c01f47ded20cc3e0
Flavio Rogerio <solebac@hotmail.com>
2023-03-04 04:06:04 -0300

    backend remove

--- backend/src/main/java/com/araxaprojetosweb/backend/repositories/ComentarioRepository.java
+++
@@ -1,9 +0,0 @@
-package com.araxaprojetosweb.backend.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.araxaprojetosweb.backend.entities.Comentario;
-
-public interface ComentarioRepository extends JpaRepository<Comentario, Long> {
-
-}
