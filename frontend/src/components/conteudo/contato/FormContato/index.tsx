ba69bf90dc77ded99a743c9b9854f59e3733112a
Flavio Rogerio <solebac@hotmail.com>
2023-03-06 16:16:43 -0300

    restore FormContato and request.ts

--- frontend/src/components/conteudo/contato/FormContato/index.tsx
+++ frontend/src/components/conteudo/contato/FormContato/index.tsx
@@ -1,45 +1,142 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import BotaoEnviar from "../BotaoEnviar";
+import { BASE_URL_NEXT } from "../../../../utils/requests";
 
 const FormContato = () => {
+  const [nome, setNome] = useState("");
+  const [email, setEmail] = useState("");
+  const [telefone, setTelefone] = useState("");
+  const [msg, setMsg] = useState("");
+  const [contato, setContato] = useState("radio-whatsapp");
+  const [receber, setReceber] = useState(false);
+  const [visible, setVisible] = useState(true);
+  //teste
+  const [assunto, setAssunto] = useState("");
+  const [radioWhatsapp, setRadioWhatsapp] = useState(true);
+  const [radioTelefone, setRadioTelefone] = useState(false);
+  const [radioEmail, setRadioEmail] = useState(false);
+
+  useEffect(() => {
+    setNome("");
+    setEmail("");
+    setTelefone("");
+    setMsg("");
+    setAssunto("");
+    setContato("radio-whatsapp");
+    setReceber(false);
+    setVisible(true);
+  }, []);
+
+  const handleFormSubmit = async (event: any) => {
+    event.preventDefault();
+    const nomeSobrenome = event.target.nomeSobrenome.value;
+
+    const header = new Headers();
+    header.append("Content-Type", "application/json");
+    header.append("Access", 'application/json"');
+    header.append("Access-Control-Allow-Origin", "*");
+
+    const rawResponse = await fetch(`${BASE_URL_NEXT}/api/sendjs`, {
+      method: "POST",
+      headers: header,
+      body: JSON.stringify({
+        nome,
+        email,
+        assunto,
+        msg,
+        telefone,
+        radioEmail,
+        radioTelefone,
+        radioWhatsapp,
+        receber,
+      }),
+    });
+    const rest = await rawResponse.json();
+    console.log(rest);
+    console.log("--------------------------");
+    console.log(rest.response);
+  };
+  const handleRadioChange = (event: any) => {
+    /*Case: With Object.: 
+    const { name, value } = event.target;
+    setRadios({
+      ...radios,
+      [name]: value
+    })*/
+    //console.log(event.target.value);
+    setContato(event.target.value);
+  };
   return (
     <>
       <section className="container celling">
         <div className="contato contato--moon contato--resize">
           <legend className="contato__titulo">Fale conosco</legend>
           <legend className="contato__subtitulo">
-            Você pode esclarecer todas as suas dúvidas sobre produtos e
-            serviços.
+            VocÃª pode esclarecer todas as suas dÃºvidas sobre produtos e
+            serviÃ§os.
           </legend>
-          <form className="contato__form">
+          <form
+            className="contato__form"
+            id="form-contact"
+            onSubmit={handleFormSubmit}
+          >
             <fieldset>
               <input
+                onChange={(event) => {
+                  setNome(event.target.value);
+                }}
                 type="text"
-                id="nomeSobrenome"
+                name="nomeSobrenome"
+                defaultValue={nome}
                 className="contato__form--input-padrao"
                 placeholder="Nome e sobrenome"
                 required
               />
               <input
+                onChange={(event) => {
+                  setEmail(event.target.value);
+                }}
                 type="email"
-                id="email"
+                name="email"
+                defaultValue={email}
                 className="contato__form--input-padrao"
                 required
                 placeholder="seuemail@dominio.com"
               />
               <input
+                onChange={(e) => {
+                  setTelefone(e.target.value);
+                }}
                 type="tel"
-                id="telefone"
+                name="telefone"
+                defaultValue={telefone}
                 className="contato__form--input-padrao"
                 required
                 placeholder="(xx)xxxxx-xxxx"
               />
+
+              <input
+                onChange={(e) => {
+                  setAssunto(e.target.value);
+                }}
+                type="text"
+                name="assunto"
+                defaultValue={assunto}
+                className="contato__form--input-padrao"
+                required
+                placeholder="Assunto"
+              />
+
               <label htmlFor="mensagem" className="contato__etiqueta">
                 Mensagem
               </label>
+
               <textarea
+                onChange={(event) => {
+                  setMsg(event.target.value);
+                }}
                 name="mensagem"
-                id="mensagem"
+                defaultValue={msg}
                 rows={3}
                 className="contato__form--input-padrao"
               ></textarea>
@@ -50,32 +147,34 @@ const FormContato = () => {
                 </legend>
                 <label htmlFor="radio-email" className="contato__etiqueta">
                   <input
+                    onChange={handleRadioChange}
                     type="radio"
                     name="contato"
-                    value="email"
-                    id="radio-email"
+                    value="radio-email"
+                    checked={contato === "radio-email"}
                     placeholder="Email"
                   />
                   Email
                 </label>
                 <label htmlFor="radio-telefone" className="contato__etiqueta">
                   <input
+                    onChange={handleRadioChange}
                     type="radio"
                     name="contato"
-                    value="telefone"
-                    id="radio-telefone"
+                    value="radio-telefone"
+                    checked={contato === "radio-telefone"}
                     placeholder="Telfone"
                   />
                   Telefone
                 </label>
                 <label htmlFor="radio-whatsapp" className="contato__etiqueta">
                   <input
+                    onChange={handleRadioChange}
                     type="radio"
                     name="contato"
-                    value="whatsapp"
-                    id="radio-whatsapp"
+                    value="radio-whatsapp"
+                    checked={contato === "radio-whatsapp"}
                     placeholder="WhatsApp"
-                    checked
                   />
                   WhatsApp
                 </label>
@@ -83,20 +182,26 @@ const FormContato = () => {
 
               <fieldset>
                 <legend className="contato__etiqueta">
-                  Qual horário prefere ser atendido?
+                  Qual horÃ¡rio prefere ser atendido?
                 </legend>
-                <select id="preferencial">
-                  <option value="">Manhã</option>
-                  <option value="">Tarde</option>
-                  <option value="">Noite</option>
+                <select id="preferencial" name="preferencial">
+                  <option value="1">ManhÃ£</option>
+                  <option value="2">Tarde</option>
+                  <option value="3">Noite</option>
                 </select>
               </fieldset>
 
               <label className="checkboks contato__etiqueta" htmlFor="receber">
-                <input type="checkbox" id="receber" />
+                <input
+                  type="checkbox"
+                  id="receber"
+                  onChange={(event) => {
+                    setReceber(event.target.checked);
+                  }}
+                />
                 Gostaria de receber nossas novidades por email?
               </label>
-              <BotaoEnviar />
+              {visible ? <BotaoEnviar /> : ""}
             </fieldset>
           </form>
         </div>
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,6 +1,10 @@
+//Heroku(Postgress)
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080"
 //export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://araxaprojetosweb.herokuapp.com";
 
 
-//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://nextappcredrapido.netlify.app";
-export const BASE_URL_EMAIL = process.env.REACT_APP_BACKEND_URL_EMAIL ?? "https://nextappcredrapido.netlify.app";
+//(Dev) -> export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://nextappcredrapido.netlify.app";
+//(Prod) ->
+export const BASE_URL_NEXT = process.env.REACT_APP_BACKEND_URL_NEXT ?? "https://nextappcredrapido.netlify.app";
+//export const BASE_URL_NEXT = process.env.REACT_APP_BACKEND_URL_NEXT ?? "http://localhost:3000";
+export const BASE_PEOPLE = "ZmzDoXZpbw==";
