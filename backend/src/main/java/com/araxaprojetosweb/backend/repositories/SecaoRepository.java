1a5e13818834439909556cd96792926e80a38048
Flavio Rogerio <solebac@hotmail.com>
2023-03-08 16:50:45 -0300

    Update mode problem select JPQL SecaoRepository.java

--- backend/src/main/java/com/araxaprojetosweb/backend/repositories/SecaoRepository.java
+++ backend/src/main/java/com/araxaprojetosweb/backend/repositories/SecaoRepository.java
@@ -16,11 +16,11 @@ public interface SecaoRepository extends JpaRepository<Secao, Integer>{
 			+ "from tb_secao tab where tab.categoria_id = :categoria_id ", nativeQuery=true)
 	List<ISecaoProjecaoSingle> findByCategoriaSingle(Integer categoria_id);
 	
-	@Query(value="select count(tab2.secao_id) as contador, tab.nome, tab.categoria_id , tab2.secao_id\r\n"
-			+ "			from tb_secao tab \r\n"
-			+ "inner join TB_CATEGORIA tab1 on(tab.categoria_id = tab1.id and tab.categoria_id = :categoria_id)\r\n"
-			+ "left join tb_artigo tab2 on(tab.id = tab2.secao_id)\r\n"
-			+ "group by tab.id\r\n"
+	@Query(value="select count(tab2.secao_id) as contador, tab.nome, tab.categoria_id , coalesce(tab2.secao_id,0) as secao_id "
+			+ "			from tb_secao tab "
+			+ "inner join TB_CATEGORIA tab1 on(tab.categoria_id = tab1.id and tab.categoria_id = :categoria_id) "
+			+ "left join tb_artigo tab2 on(tab.id = tab2.secao_id) "
+			+ "group by tab.nome, tab.categoria_id ,  secao_id "
 			+ "order by tab.nome", nativeQuery = true)
 	List<ISecaoProjecaoFull> findByCategoriaFull(Integer categoria_id);
 }
