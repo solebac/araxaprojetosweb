1dd5f250fdd596dcd4327594a99636e8744f5831
Flavio Rogerio <solebac@hotmail.com>
2023-03-11 03:23:18 -0300

    reload requests.ts

--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,6 +1,6 @@
 //Heroku(Postgress)
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080"
-//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://araxaprojetosweb.herokuapp.com";
+//export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "http://localhost:8080"
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://araxaprojetosweb.herokuapp.com";
 
 
 //(Dev) -> export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? "https://nextappcredrapido.netlify.app";
